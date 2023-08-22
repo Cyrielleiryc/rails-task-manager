ea2c2ae0799aebe1b167f8b0da5690dcd50243cb
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-08-22 12:23:29 +0200

    add 'create a task'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,24 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def set_task
-    @task = Task.find(params["id"])
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
